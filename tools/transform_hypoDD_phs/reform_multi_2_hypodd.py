6d7c73e19f247c1d9aafe299728d542117f95cc5
mensanli <mensanli@163.com>
2021-11-30 14:22:32 +0800

    mod reform_multi_2_hypodd.py

--- tools/transform_hypoDD_phs/reform_multi_2_hypodd.py
+++ tools/transform_hypoDD_phs/reform_multi_2_hypodd.py
@@ -2,11 +2,14 @@
 # code @msl
 # last mod 2021/11/29
 # 
-# mslphs (from hypoinvers / CSF): "# 2021 11 21 18 40 15.85 37.091 77.012 15 1.9 0.695 20211121184015 1"  "XJ AKZ 2021 11 21 18 40 24.82 S"
+# mslphs (from hypoinvers): "# 2021 11 21 18 40 15.85 37.091 77.012 15 1.9 0.695 20211121184015 1"  "XJ AKZ 2021 11 21 18 40 24.82 S"
 # tempphs (MFT): "# 2008 09 30 17 05 26.240 37.610 76.098 136 2.1 0 0 0.679969 1 XJ.200810010105.0003 eq"  "XJBCH 47.1 1.0 Pn -0.23"
+# CSF: CENC catalog formate
 
 from obspy.core import UTCDateTime
 import argparse
+from datetime import datetime
+import datetime as time
 
 def mslphs_DD(phsfile):
     f=open(phsfile,'r')
@@ -67,9 +70,9 @@ def tempphs_DD(phsfile):
         else:
             print("%-6s%9.2f%7.3f%4s"%(item[0],float(item[1]),float(item[2]),item[3][:1]))
 
-def CSFphs_DD(phsfile):
+def CSFphs_DD(phsfile): # Beijing Time ---> UTC Time 
     f=open(phsfile,'r',encoding='gbk')
-    n=0
+    n=6762
     for line in f.read().splitlines():
         item=line.split(" ")
         while '' in item:
@@ -81,41 +84,78 @@ def CSFphs_DD(phsfile):
                 HMS=item[3].split(":")
                 time_o=item[2]+"T"+item[3]
                 #print(time_o)
-                origin_time_str=str(time_o)
-                origin=UTCDateTime(origin_time_str)
+                #origin_time_str=str(time_o)
+                #origin=UTCDateTime(origin_time_str)   # Beijing Time
+                time_o=datetime.strptime(time_o,"%Y-%m-%dT%H:%M:%S.%f")
+                origin=time_o-time.timedelta(seconds=28800)
                 n+=1
                 nn=str(n)
-                print("%s%5s%3s%3s%4s%3s%6.2f%9.4f%9.4f%7.1f%6.2f%6s%6s%6s%10s"%("#",YMD[0],YMD[1],YMD[2],HMS[0],HMS[1],float(HMS[2]),float(item[4]),float(item[5]),float(item[6]),float(item[8]),"0.00","0.00","0.00",nn))
+                #print("%s%5s%3s%3s%4s%3s%6.2f%9.4f%9.4f%7.1f%6.2f%6s%6s%6s%10s"%("#",YMD[0],YMD[1],YMD[2],HMS[0],HMS[1],float(HMS[2]),float(item[4]),float(item[5]),float(item[6]),float(item[8]),"0.00","0.00","0.00",nn))
+                print("%s%5s%3s%3s%4s%3s%6.2f%9.4f%9.4f%7.1f%6.2f%6s%6s%6s%10s"%("#",origin.year,origin.month,origin.day,origin.hour,origin.minute,float(origin.second),float(item[4]),float(item[5]),float(item[6]),float(item[8]),"0.00","0.00","0.00",nn))
             elif item[0]=="DBO" and item[6]=="ML":
                 YMD=item[2].split("-")
                 HMS=item[3].split(":")
                 time_o=item[2]+"T"+item[3]
-                print(time_o)
-                origin_time_str=str(time_o)
-                origin=UTCDateTime(origin_time_str)
+                #print(time_o)
+                #origin_time_str=str(time_o)
+                #origin=UTCDateTime(origin_time_str)
+                time_o=datetime.strptime(time_o,"%Y-%m-%dT%H:%M:%S.%f")
+                origin=time_o-time.timedelta(seconds=28800)
                 n+=1
                 nn=str(n)
-                print("%s%5s%3s%3s%4s%3s%6.2f%9.4f%9.4f%7.1f%6.2f%6s%6s%6s%10s"%("#",YMD[0],YMD[1],YMD[2],HMS[0],HMS[1],float(HMS[2]),float(item[4]),float(item[5]),0,float(item[7]),"0.00","0.00","0.00",nn))
+                #print("%s%5s%3s%3s%4s%3s%6.2f%9.4f%9.4f%7.1f%6.2f%6s%6s%6s%10s"%("#",YMD[0],YMD[1],YMD[2],HMS[0],HMS[1],float(HMS[2]),float(item[4]),float(item[5]),0,float(item[7]),"0.00","0.00","0.00",nn))
+                print("%s%5s%3s%3s%4s%3s%6.2f%9.4f%9.4f%7.1f%6.2f%6s%6s%6s%10s"%("#",origin.year,origin.month,origin.day,origin.hour,origin.minute,float(origin.second),float(item[4]),float(item[5]),0,float(item[7]),"0.00","0.00","0.00",nn))
             elif item[0]=="DPB" and item[4]=="Pg" and item[1]=="XJ":
                 net=item[1]
                 station=item[2]
                 #weight=item[5]
                 pick="P"
                 time_pick=item[7]+"T"+item[8]
-                p_time_str=str(time_pick)
-                pickp=UTCDateTime(p_time_str)
+                #p_time_str=str(time_pick)
+                #pickp=UTCDateTime(p_time_str)
+                time_pick=datetime.strptime(time_pick,"%Y-%m-%dT%H:%M:%S.%f")
+                pickp=time_pick-time.timedelta(seconds=28800)
                 dd=pickp-origin
-                print("%-2s%-4s%9.2f%7.3f%4s"%(net,station,dd,1,pick))
+                #print(dd)
+                print("%-2s%-4s%9.2f%7.3f%4s"%(net,station,dd.total_seconds(),1,pick))
+            elif item[0]=="DPB" and item[5]=="Pg" and item[1]=="XJ":
+                net=item[1]
+                station=item[2]
+                #weight=item[5]
+                pick="P"
+                time_pick=item[8]+"T"+item[9]
+                #p_time_str=str(time_pick)
+                #pickp=UTCDateTime(p_time_str)
+                time_pick=datetime.strptime(time_pick,"%Y-%m-%dT%H:%M:%S.%f")
+                pickp=time_pick-time.timedelta(seconds=28800)
+                dd=pickp-origin
+                #print(dd)
+                print("%-2s%-4s%9.2f%7.3f%4s"%(net,station,dd.total_seconds(),1,pick))
+
             elif item[0]=="DPB" and item[4]=="Sg" and item[1]=="XJ":
                 net=item[1]
                 station=item[2]
                 #weight=item[5]
                 pick="S"
                 time_pick=item[7]+"T"+item[8]
-                s_time_str=str(time_pick)
-                pickp=UTCDateTime(s_time_str)
-                dd=pickp-origin
-                print("%-2s%-4s%9.2f%7.3f%4s"%(net,station,dd,1,pick))
+                #s_time_str=str(time_pick)
+                #picks=UTCDateTime(s_time_str)
+                time_pick=datetime.strptime(time_pick,"%Y-%m-%dT%H:%M:%S.%f")
+                picks=time_pick-time.timedelta(seconds=28800)
+                dd=picks-origin
+                print("%-2s%-4s%9.2f%7.3f%4s"%(net,station,dd.total_seconds(),1,pick))
+            elif item[0]=="DPB" and item[5]=="Sg" and item[1]=="XJ":
+                net=item[1]
+                station=item[2]
+                #weight=item[5]
+                pick="S"
+                time_pick=item[8]+"T"+item[9]
+                #s_time_str=str(time_pick)
+                #picks=UTCDateTime(s_time_str)
+                time_pick=datetime.strptime(time_pick,"%Y-%m-%dT%H:%M:%S.%f")
+                picks=time_pick-time.timedelta(seconds=28800)
+                dd=picks-origin
+                print("%-2s%-4s%9.2f%7.3f%4s"%(net,station,dd.total_seconds(),1,pick))
         else:
             continue
 
@@ -127,7 +167,7 @@ def sel_reform_method():
     parser.add_argument("-n", "--filename", help="phsfile name",  type=str, default="1")
     parser.add_argument("-o", "--outfile", help="phsfile name",  type=str, default="1")
     args = parser.parse_args()
-    print(args.type,args.filename)
+   # print(args.type,args.filename)
     phstype = args.type
     phsfile = args.filename
 
